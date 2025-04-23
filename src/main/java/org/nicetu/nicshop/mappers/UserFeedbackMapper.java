69ad4b2b2cd96cc3c8b4ac00a4abbbdc3800093b
rodiongorcatov <rodion56730@gmail.com>
2025-04-23 19:21:46 +0300

    feedback utils added

---
+++ src/main/java/org/nicetu/nicshop/mappers/UserFeedbackMapper.java
@@ -0,0 +1,26 @@
+package org.nicetu.nicshop.mappers;
+
+import org.nicetu.nicshop.domain.Photo;
+import org.nicetu.nicshop.domain.UserFeedback;
+import org.nicetu.nicshop.dto.UserFeedbackDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public interface UserFeedbackMapper {
+    static UserFeedbackDto fromUserFeedbackToDto(UserFeedback userFeedback) {
+        UserFeedbackDto userFeedbackDto = new UserFeedbackDto();
+        userFeedbackDto.setFeedback(userFeedback.getFeedback());
+        userFeedbackDto.setComment(userFeedback.getComment());
+        userFeedbackDto.setPicturesUrls(userFeedback.getPhotos().stream()
+                .map(Photo::getPictureUrl)
+                .collect(Collectors.toList()));
+        return userFeedbackDto;
+    }
+
+    static List<UserFeedbackDto> fromUserFeedbacksToDtoList(List<UserFeedback> userFeedbacks) {
+        return userFeedbacks.stream()
+                .map(UserFeedbackMapper::fromUserFeedbackToDto)
+                .collect(Collectors.toList());
+    }
+}
